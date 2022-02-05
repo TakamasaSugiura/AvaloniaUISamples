44ecb6f789ae792fbb8b299187fad7cb5eeb32ea
Takamasa Sugiura <takamasas2008@gmail.com>
2022-02-06 04:52:26 +0900

    commit

--- ImageViewer/ViewModels/MainWindowViewModel.fs
+++ ImageViewer/ViewModels/MainWindowViewModel.fs
@@ -3,15 +3,23 @@ namespace ImageViewer.ViewModels
 open ReactiveUI
 open System
 open Avalonia.Controls
+open Avalonia.Media.Imaging
 
 type MainWindowViewModel() =
     inherit ViewModelBase()
+    let mutable _imageSource:Bitmap = null
 
-    member this.Greeting = "Welcome to Avalonia!"
+    member this.ImageSource with get() = _imageSource and set(value) = this.RaiseAndSetIfChanged(&_imageSource, value) |> ignore
 
     member this.OpenCommand(window:Window) =
         let ret = this.GetFileName(window) |> Async.RunSynchronously
-        ()
+        if ret = null || ret.Length = 0 then
+            ()
+        try
+            let bmp = new Bitmap(ret.[0])
+            this.ImageSource <- bmp
+        with
+            | _ as ex -> Console.WriteLine(ex.ToString())
 
     member this.GetFileName(window:Window): Async<string[]> =
         async{
