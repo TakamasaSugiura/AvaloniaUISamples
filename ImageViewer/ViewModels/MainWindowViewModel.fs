2c68610f4719cdf7f4aa86939be7e95d127ad570
Takamasa Sugiura <takamasas2008@gmail.com>
2022-02-06 00:02:16 +0900

    commit

--- ImageViewer/ViewModels/MainWindowViewModel.fs
+++ ImageViewer/ViewModels/MainWindowViewModel.fs
@@ -10,13 +10,14 @@ type MainWindowViewModel() =
     member this.Greeting = "Welcome to Avalonia!"
 
     member this.OpenCommand(window:Window) =
-        this.GetFileName(window) |> Async.RunSynchronously
+        let ret = this.GetFileName(window) |> Async.RunSynchronously
+        ()
 
-    member this.GetFileName(window:Window) =
+    member this.GetFileName(window:Window): Async<string[]> =
         async{
             let dlg = new OpenFileDialog()
             let! fileName = dlg.ShowAsync(window) |> Async.AwaitTask
-            ()
+            return fileName
         }
 
     member this.ExitCommand(window:Window) =
